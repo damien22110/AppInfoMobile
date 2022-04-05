dd6618bee58ae4c005aeca55e661c1c2eacbe7d1
Damien <laizetdami@cy-tech.fr>
2022-04-05 17:10:36 -0400

    xml2

--- app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
+++ app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private SwipeAdapter adapter;
+    private List<Integer> list;
+    Koloda koloda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        koloda=findViewById(R.id.koloda);
+        list = new ArrayList<>();
+
+        adapter = new SwipeAdapter(this, list);
+        koloda.setAdapter(adapter);
     }

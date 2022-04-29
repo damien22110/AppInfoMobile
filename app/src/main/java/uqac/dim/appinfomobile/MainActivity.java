597c5d58f883e3e18b48b4e6041364df0bd5f80e
Thomas <thomas.lefloch@eisti.fr>
2022-04-28 22:18:14 -0400

    adding solution for standard login and register

--- app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
+++ app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
@@ -1,9 +1,12 @@
 package uqac.dim.appinfomobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.yalantis.library.Koloda;
 
 import java.util.ArrayList;
@@ -13,6 +16,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private SwipeAdapter adapter;
     private List<Integer> list;
+
+    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
     Koloda koloda;
 
     @Override
@@ -24,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new SwipeAdapter(this, list);
         koloda.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        if(currentUser == null) {
+            sendToLoginActivity();
+        }
+    }
+
+    private void sendToLoginActivity() {
+        Intent intent  = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }

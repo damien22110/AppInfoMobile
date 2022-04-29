4da7e183363129c56b603eb834f447c1da03f614
Damien <laizetdami@cy-tech.fr>
2022-04-29 01:13:52 -0400

    menu 2

--- app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
+++ app/src/main/java/uqac/dim/appinfomobile/MainActivity.java
@@ -1,6 +1,9 @@
 package uqac.dim.appinfomobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -46,6 +49,18 @@ public class MainActivity extends AppCompatActivity {
         } else {
             System.out.println("Oui");
         }
+        Button button1 = (Button) findViewById(R.id.button1);
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                textView1.setText("Voilàa le mot de passe");
+
+            }
+        });
+
     }
 }
 
--- app/src/main/java/uqac/dim/appinfomobile/Profil.java
+++ app/src/main/java/uqac/dim/appinfomobile/Profil.java
@@ -61,4 +61,5 @@ public class Profil extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil, container, false);
     }
+
 }
